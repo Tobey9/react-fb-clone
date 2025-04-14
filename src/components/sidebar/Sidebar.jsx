af7fa628d274c193963d41afd133575c21e46cfe
Toby <latebloomerguy99@gmail.com>
2025-04-15 02:32:00 +0700

    Fixing Sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -6,14 +6,15 @@ import PeopleIcon from "@mui/icons-material/People";
 import MessageIcon from "@mui/icons-material/Message";
 import StorefrontIcon from "@mui/icons-material/Storefront";
 import VideoLibraryIcon from "@mui/icons-material/VideoLibrary";
+import useStateValue from "../../context/useStateValue";
 
 export const Sidebar = () => {
+  const { state } = useStateValue();
+  const { user } = state;
+
   return (
     <div className="sidebar">
-      <SidebarRow
-        src="https://mighty.tools/mockmind-api/content/human/104.jpg"
-        title="Toby"
-      />
+      <SidebarRow src={user?.photoURL} title={user?.displayName} />
       <SidebarRow Icon={EmojiFlagsOutlinedIcon} title="Pages" />
       <SidebarRow Icon={PeopleIcon} title="Friends" />
       <SidebarRow Icon={MessageIcon} title="Messenger" />
